063a5e47f5f79e5a3212f75dad03fee45105bd15
SulJiing <naqk0406@naver.com>
2023-11-16 19:15:25 +0900

    1116

---
+++ MVVM_EMP/src/main/java/kr/or/aiai/www/MyIndex.java
@@ -0,0 +1,23 @@
+package kr.or.aiai.www;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class MyIndex extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("hello ajax");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
